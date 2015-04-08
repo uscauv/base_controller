d4c04585752afa4ef0763408314572906a435d3a
Vinnie Magro <vmagro@socaldevs.com>
2015-04-07 21:18:09 -0700

    Auto-format

--- scripts/testpub.py
+++ scripts/testpub.py
@@ -3,16 +3,18 @@
 import rospy
 from geometry_msgs.msg import Twist, Vector3
 
+
 def talker():
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
     while not rospy.is_shutdown():
         message = Twist()
-        message.linear=Vector3(0.4,0.5,0.6)
-        message.angular=Vector3(0.1,0.2,0.3)
+        message.linear = Vector3(0.4, 0.5, 0.6)
+        message.angular = Vector3(0.1, 0.2, 0.3)
         pub.publish(message)
         rate.sleep()
+
 
 if __name__ == '__main__':
     try:
